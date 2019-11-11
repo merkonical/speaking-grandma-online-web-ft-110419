dfb43b054dde50264e52cb87d0f87e111d9ce22f
Maverick Adolfo <merkonical@gmail.com>
2019-11-11 05:04:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,10 @@
 
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    
+    "NO, NOT SINCE 1938!"
   else 
+    "HUH?! SPEAK UP, SONNY!"
+  end 
 end 
 
 # Whatever you say to grandma, she should respond with
